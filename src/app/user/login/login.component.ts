8d946b8c69acc43f1b858c1f38fbfa9d5541f156
Piotrek <siwinski.piotr@outlook.com>
2017-03-28 23:58:02 +0200

    Login component refactor

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit }  from '@angular/core';
 import { Router }             from "@angular/router";
 import { User }               from "../../models/user";
-import { UserService }        from "../user.service";
 import {AuthService} from "../../core/auth.service";
+import {FormGroup, FormBuilder, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -10,20 +10,25 @@ import {AuthService} from "../../core/auth.service";
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  user: User = new User('', '', '');
-  constructor(private authService: AuthService, private router: Router) {
 
-  }
+  loginForm: FormGroup;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private forumBuilder: FormBuilder
+  ) {}
 
   ngOnInit() {
-
+    this.loginForm = this.forumBuilder.group({
+      login: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    })
   }
 
-  onLoginFormSubmit(){
-    this.authService
-      .login(this.user)
-      .subscribe(() => this.router.navigate(['/']),
-        (err)=>console.log(err)
-      );
+  onSubmit(){
+    console.log(this.loginForm.value);
+    this.authService.login(this.loginForm.value).subscribe(()=>{this.router.navigate(['/'])}, (err)=> console.log(err));
+
   }
 }
