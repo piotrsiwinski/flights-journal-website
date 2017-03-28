455fd956400bb8a2c48a278a08e180622c07db6b
Piotrek <siwinski.piotr@outlook.com>
2017-03-29 00:01:42 +0200

    Register form refactor

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit }  from '@angular/core';
 import { Router }             from "@angular/router";
-import { User }               from "../../models/user";
 import {AuthService} from "../../core/auth.service";
 import {FormGroup, FormBuilder, Validators} from "@angular/forms";
 
--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -3,6 +3,7 @@ import {User} from "../../models/user";
 import {UserService} from "../user.service";
 import {Router} from "@angular/router";
 import {AuthService} from "../../core/auth.service";
+import {FormGroup, FormBuilder, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -10,16 +11,30 @@ import {AuthService} from "../../core/auth.service";
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup
   user: User = new User('','','', '');
   userCreated = false;
   errorMessage: string;
 
-  constructor(private authService: AuthService, private router: Router) {
-  }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      email:            ['', [Validators.required]],
+      login:            ['', [Validators.required]],
+      password:         ['', [Validators.required]],
+      confirmPassword:  ['', [Validators.required]]
+    });
   }
 
+  onSubmit(){
+    console.log(this.registerForm.value);
+    this.authService.register(this.registerForm.value).subscribe(()=>this.userCreated = true, err => this.errorMessage = err.toString());
+  }
   onRegisterFormSubmit(){
     this.authService
       .register(this.user)
