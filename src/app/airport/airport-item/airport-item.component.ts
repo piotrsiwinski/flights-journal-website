f6e025282104b7e1c56b4d2be24b5ac9f7c401af
Piotr <siwinski.piotr@outlook.com>
2017-06-01 11:02:06 +0200

    fix

--- src/app/airport/airport-item/airport-item.component.ts
+++ src/app/airport/airport-item/airport-item.component.ts
@@ -1,8 +1,9 @@
-import {Component, OnInit, OnDestroy} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {AirportService} from '../airport.service';
 import {AirportViewModel} from '../../models/airport-view-model';
 import {ActivatedRoute, Router} from '@angular/router';
 import {Subscription} from 'rxjs/Rx';
+import {GoogleMapsModel} from '../../models/google-maps-model';
 
 @Component({
   selector: 'app-airport-item',
@@ -12,14 +13,15 @@ import {Subscription} from 'rxjs/Rx';
 export class AirportItemComponent implements OnInit, OnDestroy {
 
   private airportName: string;
-  private subscription: Subscription;
-  selectedAirport: AirportViewModel;
+  public subscription: Subscription;
+  public selectedAirport: AirportViewModel;
+  public googleMapsModel: GoogleMapsModel = {lat: 0, lng: 0, zoom: 3, markers: []};
+  private showMap: boolean = false;
 
-  constructor(
-    private airportService: AirportService,
-    private router: Router,
-    private activatedRoute: ActivatedRoute
-  ) { }
+  constructor(private airportService: AirportService,
+              private router: Router,
+              private activatedRoute: ActivatedRoute) {
+  }
 
   ngOnInit() {
     this.airportService.selectAirport.subscribe(airport => this.selectedAirport = airport);
@@ -27,22 +29,52 @@ export class AirportItemComponent implements OnInit, OnDestroy {
       (param: any) => this.airportName = param['name']
     );
 
+    this.airportService.getAirports(this.airportName).subscribe(this.onInitSuccess);
 
-
-    this.airportService.getAirports(this.airportName).subscribe(data => this.selectedAirport = data[0]);
   }
 
-  onClick(){
+  private onInitSuccess = (data) => {
+    this.selectedAirport = data[0];
+    console.log(`Airport details: ${this.selectedAirport}`);
+    this.googleMapsModel.markers = [];
+    this.googleMapsModel.markers.push({
+      label: this.selectedAirport.name,
+      lat: this.selectedAirport.latitude,
+      lng: this.selectedAirport.longitude
+    });
+  }
+
+  onShowClick() {
+    this.showMap = true;
+    console.log('Click works');
+  }
+
+  onClick() {
     this.router.navigate(['/airport']);
   }
 
 
-  onHideButtonClick(){
+  onHideButtonClick() {
     this.selectedAirport = null;
   }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
+  }
+
+  clickedMarker(label: string, index: number) {
+    console.log(`clicked the marker: ${label || index}`);
+  }
+
+  mapClicked($event) {
+    // this.googleMapsModel.markers.push({
+    //   lat: $event.coords.lat,
+    //   lng: $event.coords.lng
+    // });
+  }
+
+  markerDragEnd(m, $event: MouseEvent) {
+    console.log('dragEnd', m, $event);
   }
 
 }
